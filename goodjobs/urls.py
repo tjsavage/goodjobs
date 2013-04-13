3fe0051ee287e3ba6fe1e70f4b9d9d042050fad4
Taylor Savage <taylor@taylorsavage.com>
2013-04-13 13:11:15 -0700

    Got static files serving

--- goodjobs/urls.py
+++ goodjobs/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -15,9 +17,4 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-)
-
-
-urlpatterns += patterns('',
-	(r'^static/(?P<path>.*)$', 'django.views.static.server', {'document_root': settings.STATIC_ROOT}),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
