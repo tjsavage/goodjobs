4cb1555d7ed547b71cca9ea38863c40912a95378
Taylor Savage <taylor@taylorsavage.com>
2013-04-13 19:57:29 -0700

    Basic tree arrows working

--- goodjobs/static/js/splash-arrows.js
+++ goodjobs/static/js/splash-arrows.js
@@ -1,91 +1,150 @@
-var PathMaker = {
-    drawNode: function(x,y,size) {
-        var el = this.paper.circle(x,y,size);
+var PI = Math.PI;
 
-        el.attr({fill: 'blue'});
-        return el;
-    },
-    drawPath: function(pathstring) {
-        var p = this.paper.path(pathstring);
-
-        return p;
-    }
+function Path(root, initialAngle) {
+    this.root = root;
+    this.nodes = new Array();
+    this.nodes.push(this.root);
+    this.endpoint = root;
+    this.angle = initialAngle;
+    this.angles = new Array();
+    this.angles.push(initialAngle);
 }
 
-PathMaker.Node = Backbone.Model.extend({
-    initialize: function() {
+Path.prototype.addNode = function(angleChange, distance) {
+    this.angle += angleChange;
+    var nextNode = {x: this.endpoint.x + distance * Math.cos(this.angle), 
+                    y: this.endpoint.y + distance * Math.sin(this.angle)};
+    this.nodes.push(nextNode);
+    this.endpoint = nextNode;
+    this.angles.push(this.angle);
+}
 
-    },
-    x: function() { return this.get('x'); },
-    y: function() { return this.get('y'); },
-    size: function() { return 5; }
-});
-
-PathMaker.Path = Backbone.Collection.extend({
-    model: Node,
-    pathstring: function() {
-        var path = "M" + this.first().x() + "," + this.first().y();
-        this.each(function(node) {
-            path += "C" + node.x() + "," + node.y();
-        });
-        return path;
+Path.prototype.pathSequence = function() {
+    var pathSequence = []
+    pathSequence.push("M", this.root.x, this.root.y);
+    pathSequence.push("R");
+    for (var i = 1; i < this.nodes.length; i++) {
+        pathSequence.push(this.nodes[i].x, this.nodes[i].y);
     }
-});
 
-PathMaker.NodeView = Backbone.View.extend({
-    layout: null,
-    initialize: function() {
-        this.element = PathMaker.drawNode(this.model.x(),
-                        this.model.y(),
-                        this.model.size());
-        this.setElement(this.element.node);
-    },
-    render: function() {
-        this.element.attr('cx', this.model.x());
-        this.element.attr('cy', this.model.y());
-    }
-});
+    return pathSequence;
+}
 
-PathMaker.PathView = Backbone.View.extend({
-    layout: null,
-    initialize: function() {
-        this.element = PathMaker.drawPath(this.model.pathstring());
-        this.setElement(this.element.node);
-    },
-    render: function() {
-        this.model.each(function(node) {
-            var nodeView = new PathMaker.NodeView({model: node});
-            nodeView.render();
-            console.log(node.x());
-            console.log(node.y());
-        });
-    },
-    animate: function(time) {
-        var p = 1;
-    }
-});
+Path.prototype.getBranch = function() {
+    var branch = new Path(this.root, this.angle);
+    branch.nodes = new Array();
+    $.each(this.nodes, function(i, node) {
+        branch.nodes.push(node);
+        branch.endpoint = node;
+    });
+    return branch;
+}
 
-
-
-function generateRandomPath(x, y, dx, dy, steps) {
-    var prev_x = x;
-    var prev_y = y;
-    var path = new PathMaker.Path([]);
+function generateRandomPath(root, initialAngle, maxAngle, splitDistance, steps) {
+    var path = new Path(root, initialAngle);
     for(var i = 0; i < steps; i++) {
-        var direction = (Math.round(Math.random()) * 2) - 1;
-        prev_x += dx * direction;
-        prev_y += dy;
-        var node = new PathMaker.Node({x: prev_x,
-                            y: prev_y});
-        path.add(node);
+        path.addNode((Math.random() * 2 - 1) * maxAngle, splitDistance);
     }
     return path;
 }
 
-$(function() {
-    var paper = new Raphael($('#canvas-container'), 500, 500);
-    var path = generateRandomPath(100, 0, 0, 40, 10);
-    PathMaker.paper = paper;
-    var pathView = new PathMaker.PathView({model: path});
-    pathView.render();
-});
+function generateRelatedPath(basePath, sharedSteps, totalSteps, splitDistance, maxAngle) {
+    var path = new Path(basePath.root, basePath.angles[sharedSteps - 1]);
+    for(var i = 1; i < sharedSteps; i++) {
+        var newNode = {x: basePath.nodes[i - 1].x, y: basePath.nodes[i - 1].y};
+        path.nodes.push(newNode);
+        path.angles.push(basePath.angles[i])
+    }
+    path.endpoint = path.nodes[path.nodes.length - 1];
+    path.angle = basePath.angles[sharedSteps - 1];
+
+    for(var i = sharedSteps; i < totalSteps; i++) {
+        path.addNode((Math.random() * 2 - 1) * maxAngle, splitDistance);
+    }
+
+    return path;
+}
+
+function generateRandomTree(root, branches, splitDistance, initialAngle, maxAngle, branchProb) {
+    var firstPath = new Path(root, initialAngle);
+    var paths = new Array();
+    paths.push(firstPath);
+    for(var i = 0; i < branches; i++) {
+        var newPaths = [];
+        $.each(paths, function(i, path) {
+            if (Math.random() < branchProb) {
+                var childPath = path.getBranch();
+                console.log(childPath);
+                console.log(path);
+                path.addNode(Math.random() * maxAngle, splitDistance);
+                childPath.addNode((0 - Math.random()) * maxAngle, splitDistance);
+                newPaths.push(childPath);
+            }
+        });
+        paths = paths.concat(newPaths);
+    }
+    return paths;
+}
+
+function drawPath( canvas, pathstr, duration, attr, callback )
+{
+    var guide_path = canvas.path( pathstr ).attr( { stroke: "none", fill: "none" } );
+    var path = canvas.path( guide_path.getSubpath( 0, 1 ) ).attr( attr );
+    var total_length = guide_path.getTotalLength( guide_path );
+    var last_point = guide_path.getPointAtLength( 0 );
+    var start_time = new Date().getTime();
+    var interval_length = 50;
+    var result = path;        
+
+    var interval_id = setInterval( function()
+    {
+        var elapsed_time = new Date().getTime() - start_time;
+        var this_length = elapsed_time / duration * total_length;
+        var subpathstr = guide_path.getSubpath( 0, this_length );            
+        attr.path = subpathstr;
+
+        path.animate( attr, interval_length );
+        if ( elapsed_time >= duration )
+        {
+            clearInterval( interval_id );
+            if ( callback != undefined ) callback();
+            guide_path.remove();
+        }                                       
+    }, interval_length );  
+    return result;
+}
+
+window.onload = function() {
+    var HEIGHT = 480;
+    var WIDTH = 640;
+    var MAX_PATHS = 20
+    var MAX_ANGLE = PI / 6.0;
+    var SPLIT_DISTANCE = 90;
+    var STEPS = 10;
+    var MIN_SHARED = 3;
+    var OPTIONS = {
+        stroke: 'blue',
+        'stroke-width': 10,
+        'stroke-opacity': .6,
+        'arrow-end': 'classic',
+        'stroke-linecap': 'round'
+    }
+
+    var paths = new Array();
+
+    var paper = new Raphael('canvas-container', 0, HEIGHT);
+    var path = generateRandomPath({x: WIDTH / 2, y:HEIGHT}, -PI / 2.0, PI / 6.0, 60, 10);
+    drawPath(paper, path.pathSequence(), 4000, OPTIONS, function(){});
+    paths.push(path);
+    var pathDrawId = setInterval( function() {
+        var pathIndex = Math.floor(Math.random() * MAX_PATHS);
+        var parentIndex = Math.floor(Math.random() * (paths.length - 1));
+        var related = generateRelatedPath(paths[parentIndex], Math.floor(Math.random() * 5) + MIN_SHARED, STEPS, SPLIT_DISTANCE, MAX_ANGLE);
+        if (paths.length <= pathIndex) {
+            paths.push(related);
+        } else {
+            paths[pathIndex] = related;
+        }
+        drawPath(paper, related.pathSequence(), 4000, OPTIONS, function(){});
+    }, 5000);    
+};
