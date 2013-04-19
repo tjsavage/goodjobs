09567f95b682e059345c0ee3317b371dcdc844fc
Taylor Savage <taylor@taylorsavage.com>
2013-04-18 22:48:24 -0700

    Set production to not debug

--- goodjobs/settings/production.py
+++ goodjobs/settings/production.py
@@ -7,7 +7,7 @@ INSTALLED_APPS += (
 	'gunicorn',
 )
 
-ALLOWED_HOSTS = ['67.228.172.86']
+ALLOWED_HOSTS = ['67.228.172.86', 'arkk.herokuapp.com', 'arkk.co', 'www.arkk.co']
 
 BROKER_POOL_LIMIT = 1
 BROKER_URL = 'amqp://mphawjwv:qEocrL3mWtNGM2e3yO__g2fMyqkPWvLQ@tiger.cloudamqp.com/mphawjwv'
